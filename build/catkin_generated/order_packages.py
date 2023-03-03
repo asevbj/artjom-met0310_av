cbb7b3a10eefb33d1efd8972d07b2420008888a0
pig <asevbj@ttu.ee>
2023-03-03 12:02:56 +0200

    Practice session 4

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/pig/artjom-met0310_av/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/opt/ros/noetic'.split(';') if '/opt/ros/noetic' != '' else []
