a2da71ca83b6afa8d6037ce64e70003be7ed2e0e
pig <asevbj@ttu.ee>
2023-03-13 20:47:49 +0200

    Practise session 5

---
+++ build/artjom_r2d2/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/pig/artjom-met0310_av/src/artjom_r2d2
+# Build directory: /home/pig/artjom-met0310_av/build/artjom_r2d2
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
